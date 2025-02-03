43e2fe9214c6046ecc299a3e9f80be94f58b5309
uros.milosavljevic <urosmilosavljevic63@gmail.com>
2025-02-03 14:47:55 +0100

    Add core-splashscreen dependency

--- app/src/main/java/com/urosmilosavljevic/foodapp/ui/theme/Theme.kt
+++ app/src/main/java/com/urosmilosavljevic/foodapp/ui/theme/Theme.kt
@@ -44,7 +44,12 @@ fun FoodAppTheme(
         when {
             dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
                 val context = LocalContext.current
-                if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+                if (darkTheme) {
+                    dynamicDarkColorScheme(context)
+                }
+                else {
+                    dynamicLightColorScheme(context)
+                }
             }
 
             darkTheme -> DarkColorScheme
