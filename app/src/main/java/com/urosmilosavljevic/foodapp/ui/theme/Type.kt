a0471d795b537943494713f660af297a84f76e66
uros.milosavljevic <urosmilosavljevic63@gmail.com>
2025-02-04 19:14:54 +0100

    Setup fonts

--- app/src/main/java/com/urosmilosavljevic/foodapp/ui/theme/Type.kt
+++ app/src/main/java/com/urosmilosavljevic/foodapp/ui/theme/Type.kt
@@ -2,35 +2,120 @@ package com.urosmilosavljevic.foodapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.urosmilosavljevic.foodapp.R
 
-// Set of Material typography styles to start with
+val senFontFamily =
+    FontFamily(
+        Font(R.font.sen_regular),
+        Font(R.font.sen_bold, FontWeight.Bold),
+        Font(R.font.sen_extra_bold, FontWeight.ExtraBold),
+        Font(R.font.sen_medium, FontWeight.Medium),
+        Font(R.font.sen_semi_bold, FontWeight.SemiBold),
+    )
+
 val Typography =
     Typography(
+        displayLarge =
+            TextStyle(
+                fontFamily = senFontFamily,
+                fontWeight = FontWeight.ExtraBold,
+                fontSize = 41.sp,
+                letterSpacing = 0.sp,
+                lineHeight = 56.sp,
+            ),
+        displayMedium =
+            TextStyle(
+                fontFamily = senFontFamily,
+                fontWeight = FontWeight.ExtraBold,
+                fontSize = 30.sp,
+                letterSpacing = 0.sp,
+            ),
+        headlineLarge =
+            TextStyle(
+                fontFamily = senFontFamily,
+                fontWeight = FontWeight.Bold,
+                fontSize = 30.sp,
+                letterSpacing = 0.sp,
+            ),
+        headlineMedium =
+            TextStyle(
+                fontFamily = senFontFamily,
+                fontWeight = FontWeight.ExtraBold,
+                fontSize = 24.sp,
+                letterSpacing = 0.sp,
+            ),
+        headlineSmall =
+            TextStyle(
+                fontFamily = senFontFamily,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                letterSpacing = 0.sp,
+            ),
+        titleLarge =
+            TextStyle(
+                fontFamily = senFontFamily,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                letterSpacing = 0.sp,
+            ),
+        titleMedium =
+            TextStyle(
+                fontFamily = senFontFamily,
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp,
+                letterSpacing = 0.sp,
+            ),
+        titleSmall =
+            TextStyle(
+                fontFamily = senFontFamily,
+                fontWeight = FontWeight.Bold,
+                fontSize = 14.sp,
+                letterSpacing = 0.sp,
+            ),
         bodyLarge =
             TextStyle(
-                fontFamily = FontFamily.Default,
+                fontFamily = senFontFamily,
                 fontWeight = FontWeight.Normal,
                 fontSize = 16.sp,
-                lineHeight = 24.sp,
-                letterSpacing = 0.5.sp,
+                letterSpacing = 0.sp,
             ),
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-     */
+        bodyMedium =
+            TextStyle(
+                fontFamily = senFontFamily,
+                fontWeight = FontWeight.Normal,
+                fontSize = 14.sp,
+                letterSpacing = 0.sp,
+            ),
+        bodySmall =
+            TextStyle(
+                fontFamily = senFontFamily,
+                fontWeight = FontWeight.Normal,
+                fontSize = 13.sp,
+                letterSpacing = 0.sp,
+            ),
+        labelLarge =
+            TextStyle(
+                fontFamily = senFontFamily,
+                fontWeight = FontWeight.Normal,
+                fontSize = 16.sp,
+                letterSpacing = 0.sp,
+            ),
+        labelMedium =
+            TextStyle(
+                fontFamily = senFontFamily,
+                fontWeight = FontWeight.Normal,
+                fontSize = 14.sp,
+                letterSpacing = 0.sp,
+            ),
+        labelSmall =
+            TextStyle(
+                fontFamily = senFontFamily,
+                fontWeight = FontWeight.Normal,
+                fontSize = 12.sp,
+                letterSpacing = 0.sp,
+            ),
     )
