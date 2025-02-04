bb350a6622768a80a79843ab9389a11ed7c7186f
Uroš Milosavljević <67282212+uroge@users.noreply.github.com>
2025-02-04 16:48:07 +0100

    Implement splash screen (#2)
    * Add core-splashscreen dependency
    
    * Fix dependency
    
    * Add white and black colors to theme
    
    * Import app logo drawable
    
    * Create splashscreen theme
    
    * Increase minSdk
    
    * Remove logo
    
    * Generate launcher icons
    
    * Implement splash screen initial
    
    * Implement splash screen for dark mode

--- app/src/main/java/com/urosmilosavljevic/foodapp/ui/theme/Theme.kt
+++ app/src/main/java/com/urosmilosavljevic/foodapp/ui/theme/Theme.kt
@@ -15,6 +15,7 @@ private val DarkColorScheme =
         primary = Purple80,
         secondary = PurpleGrey80,
         tertiary = Pink80,
+        background = Black,
     )
 
 private val LightColorScheme =
@@ -22,6 +23,7 @@ private val LightColorScheme =
         primary = Purple40,
         secondary = PurpleGrey40,
         tertiary = Pink40,
+        background = White,
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
@@ -44,7 +46,11 @@ fun FoodAppTheme(
         when {
             dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
                 val context = LocalContext.current
-                if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+                if (darkTheme) {
+                    dynamicDarkColorScheme(context)
+                } else {
+                    dynamicLightColorScheme(context)
+                }
             }
 
             darkTheme -> DarkColorScheme
