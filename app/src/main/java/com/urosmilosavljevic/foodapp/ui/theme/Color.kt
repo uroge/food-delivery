34d2de1f15e895d776667812af0ea286c7975943
uros.milosavljevic <urosmilosavljevic63@gmail.com>
2025-02-04 19:14:48 +0100

    Setup basic color scheme

--- app/src/main/java/com/urosmilosavljevic/foodapp/ui/theme/Color.kt
+++ app/src/main/java/com/urosmilosavljevic/foodapp/ui/theme/Color.kt
@@ -2,13 +2,15 @@ package com.urosmilosavljevic.foodapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Orange = Color(0xFFFF7622)
+val OrangeLight = Color(0xFFFFE1CE)
+
+val DarkBlue = Color(0xFF121223)
 
 val White = Color(0xFFFFFFFF)
+val Gray50 = Color(0xFFF3F3F3)
+val Gray100 = Color(0xFFF0F5FA)
+val Gray200 = Color(0xFF7E8A97)
+val Gray300 = Color(0xFF646982)
+val DarkGrey = Color(0xFF32343E)
 val Black = Color(0xFF000000)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
