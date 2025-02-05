d79f87a05d39b1a8ab48b1d27ee531f21d63adb5
uros.milosavljevic <urosmilosavljevic63@gmail.com>
2025-02-05 15:27:02 +0100

    Add button variants

--- app/src/main/java/com/urosmilosavljevic/foodapp/MainActivity.kt
+++ app/src/main/java/com/urosmilosavljevic/foodapp/MainActivity.kt
@@ -3,12 +3,14 @@ package com.urosmilosavljevic.foodapp
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.urosmilosavljevic.foodapp.core.ui.components.FAButton
+import com.urosmilosavljevic.foodapp.core.ui.components.FAButtonTypes
 import com.urosmilosavljevic.foodapp.core.ui.theme.FoodAppTheme
 import android.os.Bundle
 
@@ -22,11 +24,25 @@ class MainActivity : ComponentActivity() {
         setContent {
             FoodAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    FAButton(
-                        text = "Click me",
-                        onClick = {},
-                        modifier = Modifier.padding(innerPadding),
-                    )
+                    Column {
+                        FAButton(
+                            text = "Click me".uppercase(),
+                            onClick = {},
+                            modifier = Modifier.padding(innerPadding),
+                        )
+                        FAButton(
+                            text = "Click me",
+                            onClick = {},
+                            modifier = Modifier.padding(innerPadding),
+                            type = FAButtonTypes.TEXT,
+                        )
+                        FAButton(
+                            text = "Click me",
+                            onClick = {},
+                            modifier = Modifier.padding(innerPadding),
+                            type = FAButtonTypes.OUTLINED,
+                        )
+                    }
                 }
             }
         }
--- app/src/main/java/com/urosmilosavljevic/foodapp/core/ui/components/FAButton.kt
+++ app/src/main/java/com/urosmilosavljevic/foodapp/core/ui/components/FAButton.kt
@@ -1,39 +1,74 @@
 package com.urosmilosavljevic.foodapp.core.ui.components
 
-import androidx.compose.material3.Button
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+enum class FAButtonTypes {
+    FILLED,
+    OUTLINED,
+    TEXT,
+}
 
 @Composable
 fun FAButton(
     text: String,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
+    type: FAButtonTypes = FAButtonTypes.FILLED,
 ) {
-    Button(
-        onClick = onClick,
-        modifier = modifier,
-        colors =
-            ButtonDefaults.buttonColors(
-                containerColor = MaterialTheme.colorScheme.primary,
-                contentColor = MaterialTheme.colorScheme.onPrimary,
-            ),
-        shape = MaterialTheme.shapes.small,
-    ) {
-        Text(
-            text = text.uppercase(),
-            style = MaterialTheme.typography.titleSmall,
-
-        )
+    when (type) {
+        FAButtonTypes.FILLED ->
+            FilledTonalButton(
+                onClick = onClick,
+                modifier = modifier,
+                colors =
+                    ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        contentColor = MaterialTheme.colorScheme.onPrimary,
+                    ),
+                shape = MaterialTheme.shapes.small,
+            ) {
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.titleSmall,
+                )
+            }
+        FAButtonTypes.OUTLINED ->
+            OutlinedButton(
+                onClick = onClick,
+                modifier = modifier,
+                border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
+                shape = MaterialTheme.shapes.small,
+            ) {
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.titleSmall,
+                )
+            }
+        FAButtonTypes.TEXT ->
+            TextButton(
+                onClick = onClick,
+                modifier = modifier,
+            ) {
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.titleSmall,
+                )
+            }
     }
 }
 
 @Preview
 @Composable
 private fun FAButtonPreview() {
-    FAButton(text = "Click me", onClick = {})
+    FAButton(text = "Click me".uppercase(), onClick = {}, type = FAButtonTypes.OUTLINED)
 }
