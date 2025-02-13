3bd52a36caab5b29360d98c1fa94423d7345f1c2
uros.milosavljevic <urosmilosavljevic63@gmail.com>
2025-02-13 18:22:15 +0100

    Extract AuthenticationScreenLayout to separate component

--- app/src/main/java/com/urosmilosavljevic/foodapp/authentication/presentation/LoginScreen.kt
+++ app/src/main/java/com/urosmilosavljevic/foodapp/authentication/presentation/LoginScreen.kt
@@ -1,21 +1,10 @@
 package com.urosmilosavljevic.foodapp.authentication.presentation
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.urosmilosavljevic.foodapp.authentication.domain.ValidateEmail
 import com.urosmilosavljevic.foodapp.authentication.domain.ValidatePassword
-import com.urosmilosavljevic.foodapp.authentication.presentation.components.AuthenticationScreenTitle
+import com.urosmilosavljevic.foodapp.authentication.presentation.components.AuthenticationScreenLayout
 import com.urosmilosavljevic.foodapp.authentication.presentation.components.LoginForm
 import com.urosmilosavljevic.foodapp.authentication.presentation.components.SignUpOptions
 import com.urosmilosavljevic.foodapp.core.ui.theme.FoodAppTheme
@@ -36,34 +25,16 @@ fun LoginScreen(
     onLoginSuccess: () -> Unit,
     viewModel: LoginViewModel,
 ) {
-    Column(
-        modifier =
-            Modifier
-                .fillMaxSize(),
-    ) {
-        AuthenticationScreenTitle(
-            title = "Log In",
-            subtitle = "Please sign in to your existing account",
-        )
-        Column(
-            modifier =
-                Modifier.clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
-                    .background(
-                        MaterialTheme.colorScheme.background,
-                    ),
-        ) {
-            Box(
-                modifier =
-                    Modifier
-                        .fillMaxWidth(),
-            ) {
-                Box(modifier = Modifier.padding(24.dp)) {
-                    LoginForm(onLoginSuccess, viewModel = viewModel)
-                }
-            }
+    AuthenticationScreenLayout(
+        title = "Log In",
+        subtitle = "Please sign in to your existing account",
+        primaryContent = {
+            LoginForm(onLoginSuccess, viewModel = viewModel)
+        },
+        bottomContent = {
             SignUpOptions()
-        }
-    }
+        },
+    )
 }
 
 @Preview
