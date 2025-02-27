cad29998046d37075b8a84d21152eccd43244650
uros.milosavljevic <urosmilosavljevic63@gmail.com>
2025-02-27 19:22:36 +0100

    Add location permission request UI component and icon

---
+++ app/src/main/java/com/urosmilosavljevic/foodapp/home/presentation/LocationPermissionRequest.kt
@@ -0,0 +1,71 @@
+package com.urosmilosavljevic.foodapp.home.presentation
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.urosmilosavljevic.foodapp.R
+import com.urosmilosavljevic.foodapp.core.ui.components.FAButton
+import com.urosmilosavljevic.foodapp.core.ui.theme.FoodAppTheme
+
+@Composable
+fun LocationPermissionRequest() {
+    Box(
+        modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background),
+    ) {
+        Column(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+                    .background(MaterialTheme.colorScheme.background),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.location_permission_icon),
+                contentDescription = null,
+            )
+            Spacer(
+                modifier = Modifier.height(56.dp),
+            )
+            FAButton(
+                text = "Access location".uppercase(),
+                onClick = {},
+                modifier = Modifier.fillMaxWidth(),
+            )
+            Spacer(
+                modifier = Modifier.height(32.dp),
+            )
+            Text(
+                "APP WILL ACCESS YOUR LOCATION ONLY WHILE USING THE APP",
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onSurface,
+                textAlign = TextAlign.Center,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LocationPermissionRequestPreview() {
+    FoodAppTheme {
+        LocationPermissionRequest()
+    }
+}
